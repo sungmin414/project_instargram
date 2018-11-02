3e21cf21d0f65a4445fcf61b9a3fddcfeb27df90
sungmin414 <sungmin3231@gmail.com>
2018-11-02 13:03:29 +0900

    UserAdmin의 list_display속성 추가

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import User
-# Register your models here.
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'name']
+
+
+admin.site.register(User, UserAdmin)
