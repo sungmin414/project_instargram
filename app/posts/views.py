6f20805e9fb350e5790d12fce49cbc8203a16c01
sungmin414 <sungmin3231@gmail.com>
2018-11-05 18:33:49 +0900

    PostCreateForm을 HTML구성

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from members.models import User
+from posts.forms import PostCreateForm
 from .models import Post
 
 
@@ -13,4 +15,17 @@ def post_list(request):
 
 
 def post_create(request):
-    return render(request, 'posts/post_create.html')
+    if request.method == 'POST':
+        post = Post(
+            author=User.objects.first(),
+            photo=request.FILES['photo'],
+        )
+        post.save()
+        return redirect('posts:post-list')
+    else:
+        form = PostCreateForm()
+        context = {
+            'form': form,
+        }
+
+        return render(request, 'posts/post_create.html', context)
