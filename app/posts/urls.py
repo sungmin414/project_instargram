591b68bd731e86d1118b8111ba8eba412eb0b6f7
sungmin414 <sungmin3231@gmail.com>
2018-11-05 17:54:21 +0900

    posts.views.post_create, posts/post_create.html연결 및 템플릿 form구현

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -8,4 +8,5 @@ app_name = 'posts'
 urlpatterns = [
     # posts.urls 내의 패턴들은, prefix 가 '/posts/'임
     path('', views.post_list, name='post-list'),
+    path('create/', views.post_create, name='post-create')
 ]
--- app/posts/views.py
+++ app/posts/views.py
@@ -10,3 +10,7 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'posts/post_list.html', context)
+
+
+def post_create(request):
+    return render(request, 'posts/post_create.html')
