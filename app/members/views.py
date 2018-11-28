cc7b25adeebb04aa1ed5491cdcd4c06273117cc1
sungmin414 <sungmin3231@gmail.com>
2018-11-28 20:52:19 +0900

    SignupForm에서 유효하지 않은 경우, 템플릿에서 해당에러를 출력, signup view에서 중복 render()부분 제거

--- app/members/views.py
+++ app/members/views.py
@@ -35,27 +35,27 @@ def logout_view(request):
 
 
 def signup_view(request):
+    context = {
+        'form': SignupForm(),
+    }
     if request.method == 'POST':
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
         if User.objects.filter(username=username).exists():
-            return HttpResponse(f'사용자명({username})은 이미 사용중입니다.')
-        if password1 != password2:
-            return HttpResponse('비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다')
+            context['error'] = f'사용자명({username})은 이미 사용중입니다.'
+        elif password1 != password2:
+            context['error'] = '비밀번호와 비밀번호 확인란의값이 일치하지 않습니다.'
+        else:
 
-        # create_user메서드는 create와 달리 자동으로 password해싱을 해줌
-        user = User.objects.create_user(
-            username = username,
-            password = password1,
-        )
-        login(request, user)
-        return redirect('posts:post-list')
-    else:
-        form = SignupForm()
-        context = {
-            'form': form,
-        }
+            # create_user메서드는 create와 달리 자동으로 password해싱을 해줌
+            user = User.objects.create_user(
+                username = username,
+                password = password1,
+            )
+            login(request, user)
+            return redirect('posts:post-list')
+
         return render(request, 'members/signup.html', context)
-    pass
+
